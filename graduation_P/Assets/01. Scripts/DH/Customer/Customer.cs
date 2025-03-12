12195b96f43ddd39bc847205227702581b50bb19
heedohee <ydohee0907@gmail.com>
2025-03-12 10:55:39 +0900

    tmp import

---
+++ graduation_P/Assets/01. Scripts/DH/Customer/Customer.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class Customer : MonoBehaviour
+{
+    public CustomerStats stats;
+}
---
+++ graduation_P/Assets/01. Scripts/DH/Customer/CustomerStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CustomerStats : ScriptableObject
+{
+    public int drunk = 0;
+    public int reliance = 0;
+    public int like = 0;
+}
