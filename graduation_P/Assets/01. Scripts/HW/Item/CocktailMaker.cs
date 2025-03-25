a5c1017d5088d69cf0a5fdf4bea64d2f163e2a9e
gksdnfgksdnf <kkho8489@gmail.com>
2025-03-25 21:03:17 +0900

    CocktailMaker

--- graduation_P/Assets/01. Scripts/HW/Item/CocktailMaker.cs
+++ graduation_P/Assets/01. Scripts/HW/Item/CocktailMaker.cs
@@ -1,8 +1,35 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CocktailMaker : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject obj;
+
+    private Vector3 originPosition;
+    private float moveDuration = .2f; // 이동 시간
+    private float threshold = 0.01f; // 도착 판단 거리
+
+    private void Start()
+    {
+        originPosition = obj.transform.position;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OrderMenu();
+        }
+    }
+    public void OrderMenu()
+    {
+        StartCoroutine(MoveToPosition(new Vector3(0, 0, 0)));
+    }
+    public void FinishMakeCocktail()
+    {
+        StartCoroutine(MoveToPosition(originPosition, 2));
+    }
     public void ProcessAddIngredient(Item target, Ingredient ingredient)
     {
         if (target is Tool tool)
@@ -36,4 +63,18 @@ public class CocktailMaker : MonoBehaviour
     {
         target.UseTool(tool);
     }
+
+    private IEnumerator MoveToPosition(Vector3 target, float t = 0)
+    {
+        yield return new WaitForSeconds(t);
+
+        while (Vector3.Distance(obj.transform.position, target) > threshold)
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position, target, Time.deltaTime * (1f / moveDuration));
+            yield return null;
+        }
+
+        obj.transform.position = target; // 정확한 위치 보정
+    }
+
 }
