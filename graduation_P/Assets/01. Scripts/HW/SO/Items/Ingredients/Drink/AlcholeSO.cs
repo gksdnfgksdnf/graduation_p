58c7020806f1bb9e9b8da543966c406095d6f7e9
gksdnfgksdnf <kkho8489@gmail.com>
2025-03-31 20:27:06 +0900

    UI

--- graduation_P/Assets/01. Scripts/HW/SO/Items/Ingredients/Drink/AlcholeSO.cs
+++ graduation_P/Assets/01. Scripts/HW/SO/Items/Ingredients/Drink/AlcholeSO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewAlchole", menuName = "SO/Item/Drink/Alchole")]
 public class AlcholeSO : IngredientSO
 {
-    [Range(0, 100)] public int alcoholContent;  // 도수
-    [Range(0, 10)] public int bitterness;       // 쓴맛
+    [Range(0, 60)] public int alcoholContent;  // 도수
+    [Range(0, 5)] public int bitterness;       // 쓴맛
+    [Range(0, 5)] public int sweetness;        // 단맛
+    [Range(0, 5)] public int sourness;         // 신맛
+    [Range(0, 3)] public int saltiness;        // 짠맛
 }
--- graduation_P/Assets/01. Scripts/HW/SO/Items/Ingredients/Drink/JuiceSO.cs
+++ graduation_P/Assets/01. Scripts/HW/SO/Items/Ingredients/Drink/JuiceSO.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewJuice", menuName = "SO/Item/Drink/Juice")]
 public class JuiceSO : IngredientSO
 {
-    [Range(0, 10)] public int sweetness;        // 단맛
-    [Range(0, 10)] public int sourness;         // 신맛
+    [Range(0, 5)] public int bitterness;       // 쓴맛
+    [Range(0, 5)] public int sweetness;        // 단맛
+    [Range(0, 5)] public int sourness;         // 신맛
 }
